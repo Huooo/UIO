2f3ecb4099f3a40018b332f813333f465c791613
huyan <huyan@lianlianpay.com>
2019-09-27 15:28:24 +0800

    [init]: init project by vue

--- index.js
+++
@@ -1,3 +0,0 @@
-console.log('======> npm :');
-console.log(1);
-console.log(2);
---
+++ src/components/Button/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <button id="uioi-button" class="uioi-button">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'uioi-button'
+}
+</script>
+
+<style scoped>
+.uioi-button {
+  border: none;
+  outline: none;
+
+  width: 100px;
+  height: 30px;
+  color: #fff;
+  text-align: center;
+  line-height: 30px;
+  background-color: #333;
+  border-radius: 50px;
+}
+</style>
